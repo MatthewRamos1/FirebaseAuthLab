7336baad35cf50481238e243bb2beecdfeccb83f
Matthew Ramos <mmramos309@gmail.com>
2020-03-14 21:05:55 -0400

    Created enum for log in states, created new vc file for log in view

---
+++ FirebaseAuthLab/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  FirebaseAuthLab
+//
+//  Created by Matthew Ramos on 3/14/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+enum LoginState {
+    case existingUser
+    case newUser
+}
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    private var currentState: LoginState = .existingUser
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func logInPressed(_ sender: UIButton) {
+        
+    }
+}
